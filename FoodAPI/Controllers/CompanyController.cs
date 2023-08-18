1e197e7df39662a37606e3d0df5132d6828d6567
felip <felip@IRDW0-PC>
2023-08-18 09:23:19 -0300

    Add project files.

---
+++ FoodAPI/Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodAPI.Controllers
+{
+    public class CompanyController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            
+        }
+    }
+}
